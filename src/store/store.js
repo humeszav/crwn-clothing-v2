cb2b3caf57208e989cc3f3ac9e6779ebd4440c6b
Viktor M <viktor.meszaros@plan-it.hu>
2023-10-15 13:20:25 +0200

    categories redux

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,24 @@
 
 // TODO: check how to use configureStore from redux-toolkit
 import { compose, createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+//import logger from "redux-logger";
 
 // root reducer
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [logger];
+
+const loggerMiddleware = (store) => (next) => (action) => {
+  
+  if (!action.type) {
+    console.error("action type is not defined in logger middleware");
+    return next(action);
+  }
+  next(action);
+
+};
+
+const middleWares = [loggerMiddleware];
+//const middleWares = [logger];
 
 const composedEnahncer = compose(applyMiddleware(...middleWares));
 
